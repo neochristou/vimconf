581df90e0b08ebcc02d37c2fff3822900c9a87eb
Neophytos Christou <neo@amazon.com>
2025-05-09 09:34:17 -0700

    Custom changes

--- lua/config/core/keymaps.lua
+++ lua/config/core/keymaps.lua
@@ -1,3 +1,15 @@
+vim.g.mapleader = ","
+
+vim.keymap.set("n", "<leader>1", "1gt", {})
+vim.keymap.set("n", "<leader>2", "2gt", {})
+vim.keymap.set("n", "<leader>3", "3gt", {})
+vim.keymap.set("n", "<leader>4", "4gt", {})
+vim.keymap.set("n", "<leader>5", "5gt", {})
+vim.keymap.set("n", "<leader>6", "6gt", {})
+vim.keymap.set("n", "<leader>7", "7gt", {})
+vim.keymap.set("n", "<leader>8", "8gt", {})
+vim.keymap.set("n", "<leader>9", "9gt", {})
+
 -- Save with ctrl + s
 vim.keymap.set("n", "<C-s>", ":w<CR>", {})
 
--- lua/config/plugins/linting.lua
+++ lua/config/plugins/linting.lua
@@ -1,36 +1,36 @@
 return {
-	"mfussenegger/nvim-lint",
-	lazy = true,
-	event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
-	config = function()
-		local lint = require("lint")
+    "mfussenegger/nvim-lint",
+    lazy = true,
+    event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+    config = function()
+        local lint = require("lint")
 
-		lint.linters_by_ft = {
-			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
-			javascriptreact = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
-			svelte = { "eslint_d" },
-			python = { "flake8" },
-			golang = { "golangci-lint" },
-		}
+        lint.linters_by_ft = {
+            -- javascript = { "eslint_d" },
+            typescript = { "eslint_d" },
+            -- javascriptreact = { "eslint_d" },
+            -- typescriptreact = { "eslint_d" },
+            -- svelte = { "eslint_d" },
+            python = { "flake8" },
+            -- golang = { "golangci-lint" },
+        }
 
-		local flake8 = lint.linters.flake8
-		flake8.args = {
-			"--ignore",
-			"E501,W503,D100,D101,D102,D103,D104,D105,D106,D107",
-		}
-		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+        local flake8 = lint.linters.flake8
+        flake8.args = {
+            "--ignore",
+            "E501,W503,D100,D101,D102,D103,D104,D105,D106,D107",
+        }
+        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-			group = lint_augroup,
-			callback = function()
-				lint.try_lint()
-			end,
-		})
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+            group = lint_augroup,
+            callback = function()
+                lint.try_lint()
+            end,
+        })
 
-		vim.keymap.set("n", "<leader>l", function()
-			lint.try_lint()
-		end, { desc = "Trigger linting for current file" })
-	end,
+        vim.keymap.set("n", "<leader>l", function()
+            lint.try_lint()
+        end, { desc = "Trigger linting for current file" })
+    end,
 }
