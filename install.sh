7de60edcbe2eca588adac44fbf568f43382d17bd
Neophytos Christou <neophytos.christou19@gmail.com>
2026-05-14 14:45:01 -0400

    Migrate treesitter to new API and update install script
    - Rewrite nvim-treesitter config for new API (no more nvim-treesitter.configs)
    - Simplify nvim-treesitter-textobjects config
    - Rewrite install.sh for macOS with all dependencies

--- install.sh
+++ install.sh
@@ -1,112 +1,63 @@
 #!/usr/bin/env bash
 
-install_fonts_linux() {
-    echo "installing fonts. Clone is bulky but we're only installing a single font"
-    pushd /tmp
-    git clone --filter=blob:none --sparse git@github.com:ryanoasis/nerd-fonts
-    cd nerd-fonts
-    git sparse-checkout add patched-fonts/Hack
-    ./install.sh Hack
-    popd
-}
+OK_MSG="[+]"
 
 install_brew() {
-    BREW=https://raw.githubusercontent.com/Homebrew/install/master/install
     if command -v brew > /dev/null; then
-        echo -e "${OK_MSG} Using brew package manager"
+        echo -e "${OK_MSG} Homebrew already installed"
     else
-        echo -e "${OK_MSG} Brew package manager not found."
-        while true; do
-            read -p "Install brew to continue automatic install?" yn
-            case $yn in
-                [Yy]* )
-                    if [ ! `command -v xcode-select` ]; then
-                        echo "[+] Installing xcode command line tools."
-                        xcode-select --install;
-                    fi
-                    ruby -e "$(curl -fsSL ${BREW})"
-                    break;;
-                [Nn]* )
-                    echo "[+] Please perform a manual installation"
-                    exit;;
-                * ) echo "Please answer yes or no.";;
-            esac
-        done
+        echo -e "${OK_MSG} Installing Homebrew"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
 }
 
-get_linux_dist() {
-    if [ -f /etc/os-release ]; then
-        # freedesktop.org and systemd
-        . /etc/os-release
-        OS=$NAME
-        VER=$VERSION_ID
-    elif type lsb_release >/dev/null 2>&1; then
-        # linuxbase.org
-        OS=$(lsb_release -si)
-        VER=$(lsb_release -sr)
-    elif [ -f /etc/lsb-release ]; then
-        # For some versions of Debian/Ubuntu without lsb_release command
-        . /etc/lsb-release
-        OS=$DISTRIB_ID
-        VER=$DISTRIB_RELEASE
-    elif [ -f /etc/debian_version ]; then
-        # Older Debian/Ubuntu/etc.
-        OS=Debian
-        VER=$(cat /etc/debian_version)
-    elif [ -f /etc/SuSe-release ]; then
-        # Older SuSE/etc.
-        OS=SUSE
-    elif [ -f /etc/redhat-release ]; then
-        # Red Hat
-        OS=$(sudo cat /etc/redhat-release | awk '{print $1 $2}')
-        VER=$(sudo cat /etc/redhat-release | \
-            awk '{n=split($0,a," "); print a[n-1]}')
+brew_install() {
+    if brew ls --versions "$1" > /dev/null 2>&1; then
+        echo -e "\tSkipping $1 -- already installed"
     else
-        # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
-        OS=$(uname -s)
-        VER=$(uname -r)
+        echo -e "\tInstalling $1"
+        brew install "$1"
     fi
 }
-
 
 setup_mac() {
     install_brew
 
-    echo "installing fonts"
-    brew install font-hack-nerd-font
+    echo "${OK_MSG} Installing Neovim and dependencies"
+    brew_install neovim
+    brew_install luarocks
+    brew_install tree-sitter
+    brew_install node
+    brew_install fd
+    brew_install bat
+    brew_install corkscrew
+    brew_install python@3.13
+    brew_install llvm
 
-    echo "${OK_MSG} Setting mason requirements up"
-    # https://github.com/williamboman/mason.nvim#requirements
-    for pkg in neovim; do
-        if brew ls --versions ${pkg} > /dev/null; then
-            echo -e "\t Skipping $pkg -- already installed"
-        else
-            echo -e "\t Installing $pkg"
-            brew install $pkg > /dev/null 2>/dev/null
-        fi
-    done
-}
+    echo "${OK_MSG} Installing tree-sitter CLI"
+    if ! command -v tree-sitter > /dev/null; then
+        npm install -g tree-sitter-cli --registry https://registry.npmjs.org
+    fi
 
+    echo "${OK_MSG} Installing clang-format"
+    if ! command -v clang-format > /dev/null; then
+        brew_install clang-format
+    fi
 
-setup_linux() {
-    echo "${OK_MSG} Setting things up"
-    get_linux_dist
-    # FIXME on debian
-    # apt-get install python3-pynvim
-    install_fonts_linux
+    echo "${OK_MSG} Installing fonts"
+    brew install --cask font-meslo-lg-nerd-font 2>/dev/null
+
+    echo "${OK_MSG} Installing zsh plugins"
+    brew_install zsh-syntax-highlighting
 }
 
 install() {
- uname="$(uname -s)"
+    uname="$(uname -s)"
     case "${uname}" in
-        Linux*)
-            setup_linux;;
         Darwin*)
             setup_mac;;
         *)
-            echo "Default installer may not work for you!"
-            echo "Please attempt a manual installation for neovim"
+            echo "This install script is for macOS only."
             exit 1
     esac
 }
--- lua/config/plugins/nvim-treesitter.lua
+++ lua/config/plugins/nvim-treesitter.lua
@@ -8,58 +8,29 @@ return {
             "windwp/nvim-ts-autotag",
         },
         config = function()
-            -- import nvim-treesitter plugin
-            local treesitter = require("nvim-treesitter.configs")
+            require("nvim-treesitter").setup()
 
-            -- configure treesitter
-            treesitter.setup({ -- enable syntax highlighting
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = true,
-                    -- use_languagetree = false,
-                    disable = function(_, bufnr)
-                        local buf_name = vim.api.nvim_buf_get_name(bufnr)
-                        local file_size = vim.api.nvim_call_function("getfsize", { buf_name })
-                        return file_size > 500 * 1024
-                    end,
-                },
-                -- enable indentation
-                indent = { enable = true },
-                -- ensure these language parsers are installed
-                ensure_installed = {
-                    "bash",
-                    "c",
-                    -- "css",
-                    "dockerfile",
-                    "gitignore",
-                    -- "go",
-                    -- "graphql",
-                    -- "html",
-                    -- "javascript",
-                    "json",
-                    -- "lua",
-                    "markdown",
-                    "markdown_inline",
-                    -- "prisma",
-                    "python",
-                    "query",
-                    "rust",
-                    -- "terraform",
-                    -- "tsx",
-                    "typescript",
-                    "vim",
-                    "vimdoc",
-                    "yaml",
-                },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = "<C-space>",
-                        node_incremental = "<C-space>",
-                        scope_incremental = false,
-                        node_decremental = "<bs>",
-                    },
-                },
+            require("nvim-treesitter").install({
+                "bash",
+                "c",
+                "dockerfile",
+                "gitignore",
+                "json",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "rust",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "yaml",
+            })
+
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function()
+                    pcall(vim.treesitter.start)
+                end,
             })
 
             require("nvim-ts-autotag").setup()
